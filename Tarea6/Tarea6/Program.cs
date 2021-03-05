a05630f338bcfcb3997853c98b5760bc667cbe41
Gabriel-10 <gabygarciag10@gmail.com>
2021-03-04 21:12:43 -0400

    segunda version

--- Tarea6/Tarea6/Program.cs
+++ Tarea6/Tarea6/Program.cs
@@ -7,6 +7,7 @@ namespace Tarea6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Tarea 6 - Grupo aldea del sonido");
         }
     }
 }
